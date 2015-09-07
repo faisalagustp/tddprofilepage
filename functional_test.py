d540b6b8b7c96ce757b28b74107c1c903d75fa4d
faisalagustp <faisalagustp@Faisals-MacBook-Pro.local>
2015-09-07 12:53:53 +0700

    Refactor home page view to use a template

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,9 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_read_home_profile(self):
         self.browser.get('http://localhost:8000')
         self.assertIn("Web Profile Faisal", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("Web Profile Faisal", header_text)
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
