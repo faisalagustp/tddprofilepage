996e1623aa316304dcb868b34a9f89eeed2359b8
faisalagustp <faisalagustp@Faisals-MacBook-Pro.local>
2015-09-17 10:35:43 +0700

    Make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -74,7 +75,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # toni membuka bagian todolist dalam website tersebut
-        self.browser.get('http://localhost:8000/todolist')
+        self.browser.get(self.live_server_url+'/todolist')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -114,6 +115,3 @@ class NewVisitorTest(unittest.TestCase):
         #page showing that the "komentar" on that page is: yey, waktunya berlibur
         komentar = self.browser.find_element_by_id('komentar').text
         self.assertTrue(komentar)
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
