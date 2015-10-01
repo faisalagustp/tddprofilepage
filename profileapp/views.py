d06570b4a26ae1d060ae9ed9285928589315bba7
faisalagustp <faisalagustp@Faisals-MacBook-Pro.local>
2015-10-01 11:15:08 +0700

    Penambahan fitur komentar dan list of list di halaman depan

--- profileapp/views.py
+++ profileapp/views.py
@@ -9,12 +9,28 @@ def home_page(request) :
 def activity_page(request):
     return render(request, 'activity.html')
 
+def items_to_comment(items):
+    items_number = items.count();
+    comment = ""
+    if items_number == 0:
+        comment = "yey, saatnya berlibur"
+    elif items_number < 5:
+        comment = "sibuk tapi santai"
+    else:
+        comment = "oh tidak"
+
+    return comment
+
 def todolist_page(request):
-    return render(request, 'todolist.html')
+    lists = List.objects.filter()
+    items = Item.objects.filter()
+    comment = items_to_comment(items);
+    return render(request, 'todolist.html', {'comment': comment, 'lists' : lists, 'items' : items})
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
+    comment = items_to_comment(list_.item_set.all())
+    return render(request, 'list.html', {'list': list_ , 'comment': comment})
 
 def new_list(request):
     list_ = List.objects.create()
